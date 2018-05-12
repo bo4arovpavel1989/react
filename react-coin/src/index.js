739033c64c493ef5a22c8fccfd884c062434d4e1
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2018-05-12 20:45:08 +0300

    made stage 10

--- react-coin/src/index.js
+++ react-coin/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Header from './components/common/Header';
-import List from './components/list/List'
+import List from './components/list/List';
+import NotFound from './components/notfound/Notfound';
+import Detail from './components/detail/Detail';
 import './index.css';
 
 const App = ()=>{
 	return (
-	<div>
-		<Header/>
-		
-		<List/>
-	</div>	
+	<BrowserRouter>
+		<div>
+			<Header/>
+			
+			<Switch>
+			<Route path='/' component={List} exact/>
+			<Route path='/currency/:id' component={Detail} exact/>
+			<Route component={NotFound}/>
+			</Switch>
+		</div>
+	</BrowserRouter>	
 	);	
 }
 
